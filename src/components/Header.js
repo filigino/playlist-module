1756765ea3eb8e207b8c41ce2f1ac88ff2f40008
Gino Salayo <gino.salayo@gmail.com>
2020-08-20 12:14:34 -0400

    Add Navbar

---
+++ src/components/Header.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap'
+
+const Header = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <Navbar dark expand='md'>
+            <div className='container'>
+                <NavbarBrand href='/'>
+                    <img src='assets/imgs/Strumr Logo Small.svg' alt='Strumr Logo Small' />
+                </NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className='ml-auto' navbar>
+                        <NavItem>
+                            <NavLink href='/'>
+                                <span className='navbar-headings'>Learn More</span>
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href='https://discord.com/' target='_blank' rel='noopener noreferrer'>
+                                <span className='navbar-headings'>
+                                    Discord
+                                </span>
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href='/'>
+                                <span className='navbar-headings'>
+                                    Help
+                                </span>
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </div>
+        </Navbar>
+    )
+}
+
+export default Header
